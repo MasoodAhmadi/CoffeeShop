f1225d9bb9a3d38b0b83086073b474f6f0c6f977
masood ahmadi <masood@eduix.it>
2021-09-27 10:56:27 +0300

    navbar updated

---
+++ src/files/Footer.jsx
@@ -0,0 +1,40 @@
+//import logo from "./logo.svg";
+import React from "react";
+//import Afghanistanflag from "../Images/A.jpg";
+
+function Footer() {
+  return (
+    <div
+    /*  style={{
+        color: "black",
+        backgroundImage: `url(${Afghanistanflag})`,
+        backgroundPosition: "center center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundAttachment: "fixed",
+        height: "100%",
+      }} */
+    >
+      Footer
+      <br />
+      Footer adsfasdf
+      <br />
+      Footer
+      {/*  <Jumbotron>
+        <h1 className="display-3">Hello, world!</h1>
+        <p className="lead">
+          This is a simple hero unit, a simple Jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+        <hr className="my-2" />
+        <p>
+          It uses utility classes for typography and spacing to space content
+          out within the larger container.
+        </p>
+        <p className="lead"></p>
+      </Jumbotron> */}
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/files/Main.page.jsx
@@ -0,0 +1,16 @@
+//import logo from "./logo.svg";
+import React from "react";
+import Board from "./Board";
+import Footer from "./Footer";
+
+function MainPage() {
+  return (
+    <>
+      {/*  <Header /> */}
+      <Board />
+      <Footer />
+    </>
+  );
+}
+
+export default MainPage;
