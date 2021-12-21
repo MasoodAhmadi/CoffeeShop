e1b21f36347cae0913b78a08e48c3f636e302cf1
Masood Ahmadi <masood@eduix.it>
2021-12-21 16:01:06 +0200

    rearrange folder structure

--- client/src/utils/BACKEND_URL.js
+++
@@ -1,9 +0,0 @@
-const url = "http://localhost:5000";
-const BACKEND_PIN = "api";
-
-export const end_points = {
-  getallItems: `${url}/${BACKEND_PIN}/menus`,
-  getTrainModel: "https://rata.digitraffic.fi/api/v1/live-trains?version=0",
-  getAllCities: `${url}/${BACKEND_PIN}/cities`,
-  getAllOptions: `${url}/${BACKEND_PIN}/options`,
-};
